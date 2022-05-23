91f79605b2cdc96ed13f4ab49fb3401ef14196bd
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-23 15:23:08 -0700

    added read me del .venv

--- snacks/tests.py
+++ snacks/tests.py
@@ -7,3 +7,9 @@ class snacksTests(SimpleTestCase):
     url = reverse('home')
     response = self.client.get(url)
     self.assertEqual(response.status_code, 200)
+
+  def test_home_page_status(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'home.html')
+    self.assertTemplateUsed(response, 'base.html')
