e96d3ccec8e84fff2512317404b7823f87f99b03
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-23 14:41:02 -0700

    home page/about page proof of life

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+
+class snacksTests(SimpleTestCase):
+  def test_home_page_status(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
